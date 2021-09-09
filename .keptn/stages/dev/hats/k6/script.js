108cb33ea8def387952fc39ff3517dc160d9696c
Thomas Schuetz <thomas.schuetz@dynatrace.com>
2021-09-09 16:02:47 +0200

    added source
    Signed-off-by: Thomas Schuetz <thomas.schuetz@dynatrace.com>

---
+++ .keptn/stages/dev/hats/k6/script.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { Rate } from 'k6/metrics';
+
+export let errorRate = new Rate('errors');
+export let options = {
+  thresholds: {
+    errors: ['rate<0.1'], // <10% errors
+    http_req_duration: ['p(95)<500']
+  },
+};
+
+export default function () {
+  const res = http.get('http://podtato-hats.podtatohead-dev.svc.cluster.local:9001/images/hats');
+  const result = check(res, {
+    'status is 200': (r) => r.status == 200,
+  });
+
+  errorRate.add(!result);
+}
